65841948233b06eb24c412c11ab98296b5eb286d
AlexBobryshev <darknessrider1@rambler.ru>
2019-11-08 14:51:03 +0300

    +Flyweight

---
+++ 10 - Flyweight/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        FlyweightGarage garage = new FlyweightGarage();
+        MovingCar movingCar1 = new MovingCar(garage.getCar("Ferrari", "red"));
+        MovingCar movingCar2 = new MovingCar(garage.getCar("Ferrari", "red"));
+        System.out.println(movingCar1 == movingCar2);
+        System.out.println(movingCar1.getCar() == movingCar2.getCar());
+    }
+}
+
+class Car { // flyweight objects
+    private String model;
+    private String color;
+
+    public Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
+
+class MovingCar {
+    private Car car;
+    private int x;
+    private int y;
+
+    public MovingCar(Car car) {
+        this.car = car;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+}
+
+class FlyweightGarage {
+    private Set<Car> cars = new HashSet<>();
+
+    public Car getCar(String model, String color) {
+        for (Car car : cars) {
+            if (car.getModel().equals(model) && car.getColor().equals(color)) return car;
+        }
+
+        Car car = new Car(model, color);
+        cars.add(car);
+        return car;
+    }
+}
