1b2ef90cc0cf17af192877de76e620e9f5e89cee
AlexBobryshev <darknessrider1@rambler.ru>
2019-11-10 23:36:20 +0300

    CoR complete

--- 13 - Chain of Responsibility/src/Main.java
+++ 13 - Chain of Responsibility/src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-        //
+        MessageHandler messageHandler = new UpperCaseHandler(
+                new ExclamationHandler(
+                        new PrinterHandler(null)));
+        messageHandler.handle("hi there");
+    }
+}
+
+abstract class MessageHandler {
+    MessageHandler nextHandler;
+
+    public MessageHandler(MessageHandler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+    abstract void handle(String msg);
+}
+
+class UpperCaseHandler extends MessageHandler {
+    public UpperCaseHandler(MessageHandler nextHandler) {
+        super(nextHandler);
+    }
+
+    @Override
+    void handle(String msg) {
+        nextHandler.handle(msg.toUpperCase());
+    }
+}
+
+class ExclamationHandler extends MessageHandler {
+    public ExclamationHandler(MessageHandler nextHandler) {
+        super(nextHandler);
+    }
+
+    @Override
+    void handle(String msg) {
+        nextHandler.handle(msg + "!!!");
+    }
+}
+class PrinterHandler extends MessageHandler{
+    public PrinterHandler(MessageHandler nextHandler) {
+        super(nextHandler);
+    }
+
+    @Override
+    void handle(String msg) {
+        System.out.println(msg);
     }
 }
