3e7f43b3a1c0abfae1119097ce2732f9b41d6ab3
AlexBobryshev <darknessrider1@rambler.ru>
2019-11-19 00:43:34 +0300

    Visitor complete

--- 23 - Visitor/src/Main.java
+++ 23 - Visitor/src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-        //
+        Visitor visitor = new CityVisitor();
+        Element cafe = new FoodLocation();
+        Element concert = new EntertainmentLocation();
+
+        cafe.accept(visitor);
+        concert.accept(visitor);
+    }
+}
+
+interface Visitor {
+    void visit(FoodLocation location);
+    void visit(EntertainmentLocation location);
+}
+
+class CityVisitor implements Visitor {
+    @Override
+    public void visit(FoodLocation location) {
+        System.out.println("Getting some food...");
+    }
+
+    @Override
+    public void visit(EntertainmentLocation location) {
+        System.out.println("Having fun...");
+    }
+}
+
+interface Element {
+    void accept(Visitor visitor);
+}
+
+class FoodLocation implements Element {
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this); // Double dispatch
+    }
+}
+
+class EntertainmentLocation implements Element {
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this); // Double dispatch
     }
 }
